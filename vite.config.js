fd567ad9d2c48e7eb9ef3dbac9d599165e62f28c
suzuru0611 <120362718+suzuru0611@users.noreply.github.com>
2024-08-14 22:41:57 +0800

    upload img

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,7 @@ export default defineConfig({
         theme_color: "#3eaf7c",
         icons: [
           {
-            src: "/icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icons/icon-512x512.png",
+            src: "/icon.png",
             sizes: "512x512",
             type: "image/png",
           },
