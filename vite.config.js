4516f7eb752497eb121f90758446b65918df9117
suzuru0611 <120362718+suzuru0611@users.noreply.github.com>
2024-08-14 22:45:20 +0800

    icon192 upload

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,11 @@ export default defineConfig({
         theme_color: "#3eaf7c",
         icons: [
           {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
             src: "/icon.png",
             sizes: "512x512",
             type: "image/png",
